04da45da403069ea26526baf7377f8eeca676a9e
Mazoda <oabumutlak@gmail.com>
2023-03-26 13:27:37 +0300

    Removed Uneccesory content

--- src/main/java/org/example/util/FileScanner.java
+++ src/main/java/org/example/util/FileScanner.java
@@ -1,10 +1,7 @@
 package org.example.util;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
 import java.util.Scanner;
 
 public class FileScanner {
@@ -15,13 +12,11 @@ public class FileScanner {
         try {
             if (scanner == null) {
 
-
                 String path = System.getProperty("user.dir") + "\\src\\main\\java\\org\\example\\util\\pascalcode.txt";
                 File pascalCode = new File(path);
                 System.out.println(pascalCode.getPath());
                 scanner = new Scanner(pascalCode);
             }
-
 
         } catch (FileNotFoundException e) {
             System.out.println("file not found ");
