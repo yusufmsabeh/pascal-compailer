299ca1b3c781f55f6c3e449c380c4661f35ae7ef
Yusuf Msabeh <yussef.msabeh@gmail.com>
2023-03-27 12:10:00 +0300

    feat: display tokens

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 import org.example.util.FileScanner;
 
 import java.util.ArrayList;
-
 import org.example.StaticFiles;
+import  org.example.Helpers;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,17 +13,26 @@ public class Main {
         FileScanner.readFile();
         while (FileScanner.scanner.hasNextLine()) {
             String[] words = FileScanner.nextLine().split(" ");
+
             for (String word : words) {
+                if(word.trim()=="")
+                    continue;
                     if(Helpers.isExist(word.trim())){
                         tokens.add(new Token(TokenType.KEYWORD,word,FileScanner.getLineNumber()));
-                    }else  if(Helpers.isNumeric(words)){
+                        continue;
+                    }else  if(Helpers.isNumber(word)){
                         tokens.add(new Token(TokenType.NUM_CONST,word,FileScanner.getLineNumber()));
+                        continue;
+
                     }else {
                         tokens.add(new Token(TokenType.IDENTIFIER,word,FileScanner.getLineNumber()));
+                        continue;
                     }
             }
         }
-
+    for(Token token : tokens){
+        System.out.println(token);
+    }
 
     }
 }
